2d04a5e2debdb89b75e7b798d53b993c624bf87b
nasimhkm <nasimalas.dev@gmail.com>
2025-07-20 18:47:53 +0700

    test

--- MalaccaBackend/resources/views/auth/login.blade.php
+++ MalaccaBackend/resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
       type="image/svg+xml"
     />
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link
       href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css"
       rel="stylesheet"
