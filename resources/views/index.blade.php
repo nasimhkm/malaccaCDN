0091307e2c5e37ae9ef37818322490a76a29ffce
nasimhkm <nasimalas.dev@gmail.com>
2025-07-23 21:12:47 +0700

    adding google tage manager in index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8" />
     <title>Malacca</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-585SL7GM');</script>
+<!-- End Google Tag Manager -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-P9RJKKCBGP"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
@@ -23,6 +30,10 @@
     </style>
 </head>
 <body>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-585SL7GM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <header>
       <nav id="main-nav" class="fixed top-0 left-0 w-full z-50 bg-transparent transition-colors duration-300">
         <div class="max-w-screen-xl mx-auto flex justify-between items-center px-4 py-3 relative">
