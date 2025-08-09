6d9be5203369d3c6e5f38d2f6177171c78ea62c9
nasimhkm <nasimalas.dev@gmail.com>
2025-08-09 21:13:38 +0700

    fix jurnal section

--- resources/views/kalibrasi.blade.php
+++ resources/views/kalibrasi.blade.php
@@ -161,7 +161,7 @@
                 id="hero"
                 class="relative min-h-screen bg-cover bg-center flex items-center justify-center text-white"
                 style="background-image: url('{{ asset('asset/img/kalibrasi.png') }}')"
-            >
+             >
                 <div class="absolute inset-0 bg-black/30 z-10"></div>
                 <div class="relative text-center z-30 p-4">
                     <h1 class="font-bold text-6xl md:text-8xl">kalibrasi</h1>
@@ -175,7 +175,7 @@
             <section
                 id="tentang"
                 class="min-h-screen flex flex-col md:flex-row bg-[#6c0c0d] overflow-hidden"
-             >
+              >
                 <div
                     class="flex flex-col justify-center text-justify text-white w-full md:w-1/2 px-8 py-16 md:pl-10 lg:pl-20"
                 >
@@ -266,83 +266,66 @@
             </section>
             <!--jurnal section-->
             <section
-                id="jurnal"
-                class="bg-[url('{{ asset('asset/img/jurnalseduh.png') }}')] min-h-screen bg-cover bg-center"
-             >
-                <!--left side-->
-                <div class="flex flex-col md:flex-row">
-                    <!--paragraph-->
-                    <div
-                        class="flex flex-col justify-center w-full md:w-1/2 px-8 py-32 md:pl-10 lg:pl-20"
-                     >
-                        <p
-                            class="text-justify text-white font-light text-large"
-                        >
-                            Malacca Coffee Konten micro-blog atau reels berisi
-                            cerita dari individu dengan pengalaman atau
-                            pemikiran yang inspiratif. Format narasi personal
-                            ini membangun kedekatan emosional antara cerita,
-                            ruang, dan pembaca. Konten kurasi unggahan dari
-                            website resmi Malacca yang menghimpun
-                            tulisan-tulisan panjang: mulai dari esai ringan,
-                            ulasan, hingga refleksi tematik.
-                        </p>
-                    </div>
-                    <!--article-->
-                    <div>
-                        <div
-                        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
-                         >
-                        @forelse ($articles as $article)
-                        <a
-                            href="{{ route('articles.show', $article->slug) }}"
-                            class="relative block rounded-3xl shadow-lg h-[400px] bg-cover bg-center overflow-hidden group"
-                            style="background-image: url('{{ $article->featured_image ? asset('storage/' . $article->featured_image) : asset('asset/img/default-article.jpg') }}');"
-                        >
-                            <div
-                                class="absolute inset-0 bg-black/60 group-hover:bg-transparent transition-colors duration-300"
-                            ></div>
+    id="jurnal"
+    class="bg-[url('{{ asset('asset/img/jurnalseduh.png') }}')] min-h-screen bg-cover bg-center"
+>
+    <div class="flex flex-col md:flex-row">
+        <div class="w-full md:w-1/2 flex flex-col justify-center px-8 py-16 md:pl-10 lg:pl-20">
+            <div class="mb-12">
+                <p class="text-justify text-white font-light text-large">
+                    Malacca Coffee Konten micro-blog atau reels berisi cerita
+                    dari individu dengan pengalaman atau pemikiran yang
+                    inspiratif. Format narasi personal ini membangun kedekatan
+                    emosional antara cerita, ruang, dan pembaca. Konten kurasi
+                    unggahan dari website resmi Malacca yang menghimpun
+                    tulisan-tulisan panjang: mulai dari esai ringan, ulasan,
+                    hingga refleksi tematik.
+                </p>
+            </div>
 
-                            <div
-                                class="relative z-10 p-5 flex flex-col h-full justify-end"
-                            >
-                                <h6 class="text-gray-300 font-bold text-xs">
-                                    {{ $article->category }}
-                                </h6>
-                                <h5
-                                    class="mb-2 text-2xl font-bold tracking-tight text-white"
-                                >
-                                    {{ $article->title }}
-                                </h5>
-                                <p
-                                    class="product-description line-clamp-3 mb-3 font-normal text-gray-200"
-                                >
-                                    {{ $article->description }}
-                                </p>
-                                <span
-                                    class="read-more-btn text-white font-semibold hover:underline self-start"
-                                >
-                                    Read More
-                                </span>
-                            </div>
-                        </a>
-                        @empty
-                        <div class="col-span-4 text-center text-white">
-                            <p>belum ada tulisan saat ini</p>
-                        </div>
-                        @endforelse
+            <div
+                class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+            >
+                {{-- We use ->take(3) to only loop through the first 3 articles --}}
+                @forelse ($articles->take(3) as $article)
+                <a
+                    href="{{ route('articles.show', $article->slug) }}"
+                    class="relative block rounded-2xl shadow-lg h-[350px] bg-cover bg-center overflow-hidden group"
+                    style="background-image: url('{{ $article->featured_image ? asset('storage/' . $article->featured_image) : asset('asset/img/default-article.jpg') }}');"
+                >
+                    <div
+                        class="absolute inset-0 bg-black/60 group-hover:bg-black/70 transition-colors duration-300"
+                    ></div>
+                    <div
+                        class="relative z-10 p-4 flex flex-col h-full justify-end"
+                    >
+                        <h6 class="text-gray-300 font-bold text-xs">
+                            {{ $article->category }}
+                        </h6>
+                        <h5
+                            class="mb-2 text-xl font-bold tracking-tight text-white"
+                        >
+                            {{ $article->title }}
+                        </h5>
                     </div>
-                    </div>
+                </a>
+                @empty
+                <div class="col-span-full text-center text-white">
+                    <p>belum ada tulisan saat ini</p>
                 </div>
-                <!--right side-->
-                <div>
-                    {{-- Path diubah menggunakan helper asset() --}}
-                    <img
-                        src="{{ asset('asset/kalibrasi/jurnalseduh.svg') }}"
-                        alt="Jurnal Seduh"
-                    />
-                </div>
-            </section>
+                @endforelse
+            </div>
+        </div>
+
+        <div class="w-full md:w-1/2 flex items-center justify-center p-8">
+            <img
+                src="{{ asset('asset/kalibrasi/jurnalseduh.svg') }}"
+                alt="Jurnal Seduh"
+                class="w-3/4 md:w-2/3"
+            />
+        </div>
+    </div>
+</section>
             <!--temurasa section-->
             <section id="temurasa">
                 <div class="relative h-screen">
